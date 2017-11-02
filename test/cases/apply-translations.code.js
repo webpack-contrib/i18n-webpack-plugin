149a9cafd954e33936a343921fae823899ab44b6
Scott Canoni <scottcanoni@github.com>
2017-11-01 22:28:29 -0400

    Adding unit tests for template string translations

--- test/cases/apply-translations.code.js
+++ test/cases/apply-translations.code.js
@@ -1,4 +1,6 @@
 /* globals __ */
+// Define the variable to be used in the template string during translation:
+const variableForTemplateString = 'some value'; // eslint-disable-line no-unused-vars
 exports.defaults = __('default value', 'missing-key1');
 exports.missingKey = __('missing-key2');
 exports.staticKey = __('static-key');
